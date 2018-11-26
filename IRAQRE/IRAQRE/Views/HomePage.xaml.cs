73c586bc71034bfd06d0b3237f0802ca99ffaa3d
QS09 <xamarin.ninja1@gmail.com>
2018-11-26 15:51:46 +0530

    changes

--- IRAQRE/IRAQRE/Views/HomePage.xaml.cs
+++ IRAQRE/IRAQRE/Views/HomePage.xaml.cs
@@ -14,26 +14,9 @@ namespace IRAQRE.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
-        public ObservableCollection<LanguagePickerModel> languagePickerItems = new ObservableCollection<LanguagePickerModel>();
 		public HomePage ()
 		{
 			InitializeComponent ();
-            languagePickerItems.Add(new LanguagePickerModel
-            {
-                LanguageName = "Hindi",
-                //FlagUrl = "https://www.countryflags.io/in/flat/64.png"
-            });
-            languagePickerItems.Add(new LanguagePickerModel
-            {
-                LanguageName = "Chinese",
-                //FlagUrl = "https://www.countryflags.io/cn/flat/64.png"
-            });
-            languagePickerItems.Add(new LanguagePickerModel
-            {
-                LanguageName = "Kurdish",
-                //FlagUrl = "https://www.countryflags.io/iq/flat/64.png"
-            });
-            LanguagePicker.ItemsSource = languagePickerItems;
         }
 	}
 }

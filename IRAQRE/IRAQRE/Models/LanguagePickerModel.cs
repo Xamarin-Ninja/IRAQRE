d77e4c6163f87d10dbe9a8715acc69d6200544ec
QS09 <xamarin.ninja1@gmail.com>
2018-11-26 15:17:53 +0530

    picker added

---
+++ IRAQRE/IRAQRE/Models/LanguagePickerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRAQRE.Models
+{
+    public class LanguagePickerModel
+    {
+        //public string FlagUrl
+        //{
+        //    get;
+        //    set;
+        //}
+        public string LanguageName
+        {
+            get;
+            set;
+        }
+    }
+}
--- IRAQRE/IRAQRE/Views/HomePage.xaml.cs
+++ IRAQRE/IRAQRE/Views/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using IRAQRE.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,26 @@ namespace IRAQRE.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
+        public ObservableCollection<LanguagePickerModel> languagePickerItems = new ObservableCollection<LanguagePickerModel>();
 		public HomePage ()
 		{
 			InitializeComponent ();
-		}
+            languagePickerItems.Add(new LanguagePickerModel
+            {
+                LanguageName = "Hindi",
+                //FlagUrl = "https://www.countryflags.io/in/flat/64.png"
+            });
+            languagePickerItems.Add(new LanguagePickerModel
+            {
+                LanguageName = "Chinese",
+                //FlagUrl = "https://www.countryflags.io/cn/flat/64.png"
+            });
+            languagePickerItems.Add(new LanguagePickerModel
+            {
+                LanguageName = "Kurdish",
+                //FlagUrl = "https://www.countryflags.io/iq/flat/64.png"
+            });
+            LanguagePicker.ItemsSource = languagePickerItems;
+        }
 	}
 }
